142116547d6400c2b8d00d7abad4f5b5acf2ef9a
jmpleo <totrash@duck.com>
2023-12-22 22:26:53 +0300

    add roles

--- sql/mainsql-gen.sh
+++ sql/mainsql-gen.sh
@@ -3,8 +3,8 @@
 echo "add... ./table.sql"
 cat ./table.sql > main.sql
 
-#echo "add ... ./role.sql"
-#cat ./role.sql >> main.sql
+echo "add ... ./role.sql"
+cat ./role.sql >> main.sql
 
 echo "add.. ./old-db-consistency.sql"
 cat ./old-db-consistency.sql >> main.sql
--- sql/reset-db.sh
+++ sql/reset-db.sh
@@ -7,5 +7,8 @@ fi
 
 pg_dump -d $1 -f "$1.backup" && \
 psql -c "drop database $1" && \
+psql -c "drop role admin" && \
+psql -c "drop role labler" && \
+psql -c "drop role auditor" && \
 psql -c "create database $1 owner $(whoami)" && \
 psql -d $1 -f "./init.sql"
