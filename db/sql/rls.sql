cf5f2a6a676aedec3d4b0264de73a80e9f6d50b3
jmpleo <totrash@duck.com>
2023-12-24 17:55:21 +0300

    fix sql typo

--- db/sql/rls.sql
+++ db/sql/rls.sql
@@ -1,20 +1,20 @@
 ALTER TABLE sensorslines ENABLE ROW LEVEL SECURITY;
 ALTER TABLE zones ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY view_sensor_1 ON sensorslines FOR
+CREATE POLICY sensorslines_labler_sensor_1_view ON sensorslines FOR
     SELECT TO sensorslines_labler_sensor_1 USING (sensorid = 1);
 
-CREATE POLICY view_sensor_1 ON zones FOR
+CREATE POLICY zones_labler_sensor_1_view ON zones FOR
     SELECT TO zones_labler_sensor_1 USING (sensorid = 1);
 
-CREATE POLICY view_all ON sensorslines FOR
+CREATE POLICY admin_view_sensorslines ON sensorslines FOR
     ALL TO admin USING (TRUE);
 
-CREATE POLICY view_all ON zones FOR
+CREATE POLICY admin_view_zones ON zones FOR
     ALL TO admin USING (TRUE);
 
-CREATE POLICY view_sensor_1 ON sensorslines FOR
+CREATE POLICY auditor_sensor_1_view_sensorlines ON sensorslines FOR
     SELECT TO auditor_sensor_1 USING (sensorid = 1);
 
-CREATE POLICY view_sensor_1 ON zones FOR
+CREATE POLICY auditor_sensor_1_view_zones ON zones FOR
     SELECT TO auditor_sensor_1 USING (sensorid = 1);
